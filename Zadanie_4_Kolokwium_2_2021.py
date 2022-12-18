4fb538e4618cb20589c35917c72d1fbce20b4bed
Dominik393 <Dominiksmykow@wp.pl>
2022-12-18 23:47:07 +0100

    Changes by dawid

--- Zadanie_4_Kolokwium_2_2021.py
+++ Zadanie_4_Kolokwium_2_2021.py
@@ -1,7 +1,7 @@
 '''W tablicy o rozmiarze N ×N wypełnionej liczbami naturalnymi umieszczono dokładnie jeden fragment 
 ciągu Fibonacciego o długości co najmniej 3 elementów. Cały fragment leży w jednym z wierszy
 lub kolumn w kierunku rosnącym lub malejącym. Prosze napisać funkcję, która dla zadanej tablicy
-odszuka ten fragment i zwróci jego wartość.'''''
+odszuka ten fragment i zwróci jego wartość.''''
 
 def fib(n1,n2,n3):
     # f0=0
